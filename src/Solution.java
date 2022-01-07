1e552321c80e24c74b3369a0e6766215184e022b
Raving-hash <Raving-hash@foxmail.com>
2022-01-07 17:32:10 +0800

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+class Solution {
+    public int[] levelOrder(TreeNode root) {
+        if (root == null) {
+            return new int[0];
+        }
+        Queue<TreeNode> queue = new LinkedList<TreeNode>() {{
+            add(root);
+        }};
+        ArrayList<Integer> res = new ArrayList<>();
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            res.add(node.val);
+            if (node.left != null) {
+                queue.offer(node.left);
+            }
+            if (node.right != null) {
+                queue.offer(node.right);
+            }
+        }
+        int[] ints = new int[res.size()];
+        int index = 0;
+        for (int i :
+                res) {
+            ints[index++] = i;
+        }
+        return ints;
+    }
+}
