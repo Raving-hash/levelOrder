38e637e527bed5134e19d9ecc4b33bf7247e9de5
Raving-hash <Raving-hash@foxmail.com>
2022-01-07 18:09:23 +0800

    用List<List<Integer>>表示二维数组

--- src/Solution.java
+++ src/Solution.java
@@ -1,5 +1,5 @@
-import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 class TreeNode {
@@ -12,31 +12,57 @@ class TreeNode {
     }
 }
 
+//class Solution {
+//    public int[] levelOrder(TreeNode root) {
+//        if (root == null) {
+//            return new int[0];
+//        }
+//        Queue<TreeNode> queue = new LinkedList<TreeNode>() {{
+//            add(root);
+//        }};
+//        ArrayList<Integer> res = new ArrayList<>();
+//        while (!queue.isEmpty()) {
+//            TreeNode node = queue.poll();
+//            res.add(node.val);
+//            if (node.left != null) {
+//                queue.offer(node.left);
+//            }
+//            if (node.right != null) {
+//                queue.offer(node.right);
+//            }
+//        }
+//        int[] ints = new int[res.size()];
+//        int index = 0;
+//        for (int i :
+//                res) {
+//            ints[index++] = i;
+//        }
+//        return ints;
+//    }
+//}
+
 class Solution {
-    public int[] levelOrder(TreeNode root) {
-        if (root == null) {
-            return new int[0];
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new LinkedList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        if (root == null){
+            return new LinkedList<>();
         }
-        Queue<TreeNode> queue = new LinkedList<TreeNode>() {{
-            add(root);
-        }};
-        ArrayList<Integer> res = new ArrayList<>();
-        while (!queue.isEmpty()) {
-            TreeNode node = queue.poll();
-            res.add(node.val);
-            if (node.left != null) {
-                queue.offer(node.left);
+        queue.offer(root);
+        while (!queue.isEmpty()){
+            LinkedList<Integer> li = new LinkedList<>();
+            for (int i = queue.size(); i > 0 ; i--){
+                TreeNode node = queue.poll();
+                li.add(node.val);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
             }
-            if (node.right != null) {
-                queue.offer(node.right);
-            }
+            res.add(li);
         }
-        int[] ints = new int[res.size()];
-        int index = 0;
-        for (int i :
-                res) {
-            ints[index++] = i;
-        }
-        return ints;
+        return res;
     }
 }
